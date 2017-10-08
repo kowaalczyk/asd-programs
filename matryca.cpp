e09c95155ff4932980a4bf36ab6f2d46be9bb014
Krzysztof Kowalczyk <k.kowaalczyk@gmail.com>
2017-10-08 11:58:57 +0200

    Initial commit

---
+++ matryca.cpp
@@ -0,0 +1,13 @@
+#include <iostream>
+using namespace std;
+
+int main() {
+  //Jezeli mamy we wzorze AB (obok siebie), to matryca musi byc dlugosci wzoru
+  //1 gwiazdka ==> matryca krotsza o 1 od dlugosci wzoru (itd.)
+  //ale sa przypadki szczegolne: same A, same B, same *
+  //odp: n-m+1
+
+  
+
+  return 0;
+}
