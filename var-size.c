7963956020d344009e3469d336d2221ad85f25d7
Krzysztof Kowalczyk <k.kowaalczyk@gmail.com>
2017-10-08 15:41:51 +0200

    Add variable size checker util

---
+++ var-size.c
@@ -0,0 +1,8 @@
+#include <stdio.h>
+
+int main() {
+  printf("Size of int: %d\n", sizeof(int));
+  printf("Size of long long int: %d\n", sizeof(long long int));
+  printf("Size of short int: %d\n", sizeof(short int));
+  return 0;
+}
