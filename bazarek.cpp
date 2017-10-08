b69d155c6307b943d7a34b649f85c0afd218dbf4
Krzysztof Kowalczyk <k.kowaalczyk@gmail.com>
2017-10-08 19:00:27 +0200

    Cleanup

--- bazarek.cpp
+++ bazarek.cpp
@@ -83,56 +83,6 @@ int main() {
     } else {
       cout<<price_sum<<endl;
     }
-
-    //prev version
-//     long long current_price = 0;
-//     int current_products = 0;
-//     int smallest_odd = 0;
-//     int smallest_even = 0;
-//
-//    int j;
-//    for(j = n-1; j >= 0 && current_products < products_to_buy; j--) {
-//       int p = price[j];
-//
-//      current_price += p;
-//      current_products++;
-//
-//      if(p % 2) {
-//        smallest_odd = p;
-//      } else {
-//        smallest_even = p;
-//      }
-//    }
-//
-//    if(!(current_price % 2)) {
-//       int biggest_odd = 0;
-//       int biggest_even = 0;
-//
-//      for(int k=0; k<=j; k++) {
-//         int p = price[k];
-//        if(p % 2) {
-//          biggest_odd = p;
-//        } else {
-//          biggest_even = p;
-//        }
-//      }
-//
-//      if(biggest_odd - smallest_even > biggest_even - smallest_odd) {
-//        if(biggest_odd != 0 && smallest_even != 0) {
-//          current_price += biggest_odd - smallest_even;
-//        }
-//      } else {
-//        if(biggest_even != 0 && smallest_odd != 0) {
-//          current_price += biggest_even - smallest_odd;
-//        }
-//      }
-//    }
-//
-//    if(current_products == products_to_buy && current_price % 2) {
-//      cout<<current_price<<endl;
-//    } else {
-//      cout<<"-1"<<endl;
-//    }
   }
 
   return 0;
