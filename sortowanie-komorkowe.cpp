85592981af359d71bbe51a1b4521f2cd207c89a6
Krzysztof Kowalczyk <k.kowaalczyk@gmail.com>
2017-10-14 12:04:23 +0200

    Correct skeleton for sortowanie-komorkowe

--- sortowanie-komorkowe.cpp
+++ sortowanie-komorkowe.cpp
@@ -16,10 +16,46 @@
  *              - dla n liczbowych: z ilu prefixów można utworzyć [i, ..., n+i-1]? Trzeba spojrzeć na 2 ostatnie przedziały, zobaczyć które z nich sa możliwe w danej sytuacji i zsumować możliwe liczby
  * 3. Wszystkie działania robimy mod 10^9
  *
- * -- Dynamiczne rozwiązanie dla 562143
+ * -- Dynamiczne rozwiązanie dla 562143 - w liniowej pamięci:
+ * - a[i+1] = {jeśli pierwszy element z poprzednich jest mniejszy od bieżącego el}
  * - mamy 5 idących w prawo i 5 idących w lewo przedziałów 2-elementowych
  * - w prawą 31, 64 nie mogą powstać - cache: 0 1 1 1 0
  * - w lewą: 0 1 1 1 0
  * - 3-liczbowe: >>: 0, 2, 2, 0 (4 nie może być ani po 6 ani po 5) <<: 0, 2, 2(może być po 5 i po 6 więc sumujemy obie 1), 0(nie może być po 4, a po pozostałej cyfrze wychodz 0)
  * - itd.
  */
+
+#include <iostream>
+
+using namespace std;
+
+int main() {
+    int tab[1000];
+    int n;
+    cin >> n;
+    for(int i=0; i<n; i++) {
+        cin>>tab[i];
+    }
+
+    int l_cache[1000][1000];
+    int r_cache[1000][1000];
+
+    //TODO: calculate cache values for 2-sequences
+
+
+    //TODO: dynamically calculate values for longer sequences
+    for(int start = 0; start < n; start++) {
+        for(int end = 0; end < n; end++) {
+            //r_cache[start..end]: - # of (start-end)-sequences ending with tab[end]
+                //add r_cache[start..end-1] if tab[end-1] < tab[end]
+                //add l_cache[start..end-1] if tab[start] < tab[end]
+            //l_cache[start..end]: - # of (start-end)-sequences beginning with tab[start]
+                //add l_cache[start+1..end] if tab[start] < tab[start+1]
+                //add r_cache[start+1..end] if tab[start] < tab[end]
+        }
+    }
+
+    //TODO: Return the answer
+
+    return 0;
+}
