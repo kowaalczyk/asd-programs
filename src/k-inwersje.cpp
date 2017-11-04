49edc59de9a84f0644dd298b677bfa8cfbba4430
Krzysztof Kowalczyk <k.kowaalczyk@gmail.com>
2017-11-04 15:28:19 +0100

    k-inwersje 44/100, weird bugs still present

--- src/k-inwersje.cpp
+++ src/k-inwersje.cpp
@@ -1,166 +1,128 @@
 #include <iostream>
-#include <cmath>
 
 using namespace std;
 
-/* dynamicznie:
- * chcemy dla każdego elementu spamiętywać ile zaczyna się w nim k-1 inwersji
- * wtedy możemy przelecieć po elementach tablicy i policzyć mniejsze elemnety
- * na odpowiednich pozycjach w czasie O(k*n^2)
- *
- * chcemy jednak mieć czas O(n*k*log(n)):
- * tworzymy sobie drzewo przdziałowe które pamięta
- * liczbę elemntów mniejszych od elementu na danej pozycji
- *
- * drzewo przedziałowe:
- * - zmiana wartoći w węźle w czasie O(logn)
- * - implementacja w tablicy mającej 2n-1 elementów,
- * - przechodzenie podobnie jak kopiec, ale lepiej indeksować indeksy od liści
- *
- * możemy liczyć sumę od k do j:
- * - policzyć sumę [0..k] i [0..j] i odjąć
- * - sumę od zera do j liczymy idąc do góry z j liścia
- *   i dodając wartość poddrzewa przy przejsciu w lewo
- *
- * zaczynamy od drzewa z jedynkami w liściach
- * indeks liścia drzewa := wartość w tablicy danych
- * w liściach drzewa przedziałowego pamiętamy k-1 inwersje
- * (pozycja liścia oznacza pozycję pierwszego elementu inwersji)
- * - chcemy policzyć ile jest k inwersji:
- * - n razy pytamy się ile wynosi liczba elementów mniejszych niż dany
- * - n razy chcemy ustawić element na zero (nie liczymy danych elementów 2 razy)
- * - drzewo można zbudować w czasie n*log(n)
- * - powyższe kroki powtarzamy k razy
- *
- * W każdym momencie potrzeba tylko 1 drzewo, ale rozwiązanie z 2 drzewami też jest ok
- * */
+#define MOD_BIL %1000000000
 
-// drzewo przedziałowe
+// INTERVAL TREE IMPLEMENTATION
+
 // assuming 1 is the index of root, [tree_size/2...tree_size-1] are leaves
-int tree[40000];
-int tree_size;
+unsigned int tree[160000];
+unsigned int tree_size;
 
-// helper functions
+// tree helpers
 
-void debug_print_tree() {
-//    comment-out for debugging:
-//    return;
-
-    int breaker = 0;
-    int counter = 0;
-
-    for(int i=1; i< tree_size; i++) {
-        cout << tree[i] << ' ';
-        counter++;
-        if(counter >= (int)pow((float)2, (float)breaker)) {
-            cout << endl;
-            breaker++;
-            counter = 0;
-        }
-    }
-}
-
-int tree_pos(int leaf_pos) {
+unsigned int tree_pos(unsigned int leaf_pos) {
     return tree_size/2 + leaf_pos;
 }
 
-int tree_parent_pos(int node_pos) {
+unsigned int tree_parent_pos(unsigned int node_pos) {
     return node_pos/2;
 }
 
-int tree_lson_pos(int node_pos) {
+unsigned int tree_lson_pos(unsigned int node_pos) {
     return node_pos*2;
 }
 
-int tree_rson_pos(int node_pos) {
+unsigned int tree_rson_pos(unsigned int node_pos) {
     return 1 + node_pos*2;
 }
 
 // tree interface
-void tree_update_parents(int node_pos) {
+void tree_update_parents(unsigned int node_pos) {
     if(node_pos > 1) {
-        int parent_pos = tree_parent_pos(node_pos);
-        tree[parent_pos] = tree[tree_lson_pos(parent_pos)] + tree[tree_rson_pos(parent_pos)];
+        unsigned int parent_pos = tree_parent_pos(node_pos);
+        tree[parent_pos] = (tree[tree_lson_pos(parent_pos)] MOD_BIL  + tree[tree_rson_pos(parent_pos)] MOD_BIL) MOD_BIL;
         tree_update_parents(parent_pos);
     }
 }
 
 void tree_reset() {
-    for(int i=tree_pos(0); i<=tree_pos(tree_size/2); i++) {
-        tree[i] = 1;
-//        TODO: Do it in O(n) - update parents after the loop
-        tree_update_parents(i);
+    for(int i=0; i<40000; i++) {
+        tree[i] = 0;
     }
 }
 
-void tree_create(int leaves) {
-    tree_size = 2*leaves;
+void tree_create(unsigned int leaves) {
+    tree_size = 8*leaves;
     tree_reset();
 }
 
-void tree_set(int leaf_pos, int val) {
-    int pos = tree_pos(leaf_pos);
-    tree[pos] = val;
+void tree_set(unsigned int leaf_pos, unsigned int val) {
+    unsigned int pos = tree_pos(leaf_pos);
+    tree[pos] = val MOD_BIL;
     tree_update_parents(pos);
 }
 
-// for leaf j, this will return sum of values from leaves [0..j]
-int leaf_sum(int leaf_pos) {
-    // init the sum with leaf value
-    int pos = tree_pos(leaf_pos);
-    int prev_pos = -1;
-    int sum = 0;
+// for leaf_pos j, this will return sum of values from leaves at positions [0..j-1]
+unsigned int leaf_sum(unsigned int leaf_pos) {
+    unsigned int sum = 0;
 
-    prev_pos = pos;
+    unsigned int pos = tree_pos(leaf_pos);
+    unsigned int prev_pos = pos;
     pos = tree_parent_pos(pos);
     while(pos>=1) {
         // add sum from left subtree if we have moved to the left
         if(prev_pos != tree_lson_pos(pos)) {
-            sum += tree[tree_lson_pos(pos)];
+            sum += tree[tree_lson_pos(pos)] MOD_BIL;
         }
 
         prev_pos = pos;
         pos = tree_parent_pos(pos);
     }
-    return sum;
+    return sum MOD_BIL;
 }
 
-int main() {
-    int n, k;
-    int tab[20000];
+// 2lists: one contains current_k-inversions, second one - (current_one-1)-inversions
+// tree is initialized to zeros for every level
+// iterating from r too l, we count the current_k from the tree and place
+// and place (current_k-1)-inversions in its place (so that it can be counted by following iterations in a loop)
 
-//    TODO: Uważać na indeksowanie od 1 w treści zadania
-//    TODO: Wszystkie operacje mod 10^9
+int main() {
+    unsigned int n, k;
+    unsigned int tab[20000];
+    unsigned int new_leaves[20000];
+    unsigned int old_leaves[20000];
+    unsigned int k_inversions = 0;
 
     cin >> n >> k;
-    for(int i=0; i<n; i++) {
+    for(unsigned int i=0; i<n; i++) {
         cin >> tab[i];
     }
-
+    for(unsigned int i=n; i<20000; i++) {
+        tab[i] = 0;
+    }
     tree_create(n);
-//    tree_reset();
-
-    debug_print_tree();
-
-    for(int current_k=2; current_k<=k; current_k++) {
-//        cout << "current_k = " << current_k << endl;
-        int new_leaves[20000];
-
-        debug_print_tree();
-
-        for(int i=n-1; i>=0; i--) {
-            int val = tab[i];
-            new_leaves[val-1] = leaf_sum(val-1);
-//            tree_set(val-1, 0);
-        }
-        for(int i=0; i<n; i++) {
-//            cout << new_leaves[i] << ' ';
-            tree_set(i, new_leaves[i]);
-        }
-//        cout << endl;
+    for(unsigned int i=0; i<20000; i++) {
+        old_leaves[i] = 1;
     }
 
-    cout << tree[1] << endl;
+    /*
+     * new_leaves[i] := (current_k)-inversions where i is the smallest number
+     * old_leaves[i] := (current_k-1)-inversions where i is the smallest number
+     */
+    for(unsigned int current_k=2; current_k<=k; current_k++) {
+        tree_reset();
+        for(unsigned int i=n; i>0; i--) {
+            unsigned int val = tab[i-1];
+            // calculate (current_k)-inversions and store it in new leaves
+            new_leaves[val-1] = leaf_sum(val-1) MOD_BIL;
+            // add number of (current_k-1)-inversions to the tree so that the following numbers can count it
+            tree_set(val-1, old_leaves[val-1]);
+        }
+        if(current_k == k) {
+            // calculate sum of (k)-inversions from new_leaves array
+            for(unsigned int i=0; i<20000; i++) {
+                k_inversions += new_leaves[i] MOD_BIL;
+            }
+        } else {
+            // update old leaves
+            for(unsigned int i=0; i<20000; i++) {
+                old_leaves[i] = new_leaves[i] MOD_BIL;
+            }
+        }
+    }
 
+    cout << k_inversions MOD_BIL << endl;
     return 0;
 }
