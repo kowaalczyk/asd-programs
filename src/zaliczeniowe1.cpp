1a8e316be4565c30f9f9c2752d07e5157f618c94
Krzysztof Kowalczyk <k.kowaalczyk@gmail.com>
2017-11-08 18:15:21 +0100

    Skeleton for optimal implementation

--- src/zaliczeniowe1.cpp
+++ src/zaliczeniowe1.cpp
@@ -3,17 +3,17 @@
 using namespace std;
 
 
-//typedef int tree_val_t;
-//typedef int tree_pos_t;
-//
-//static const tree_pos_t TREE_MAX_SIZE = 800008; //TODO: Double-check
-//static const tree_pos_t N_MAX_SIZE = 100001;
+typedef int tree_val_t;
+typedef int tree_pos_t;
+
+static const tree_pos_t TREE_MAX_SIZE = 800008; //TODO: Double-check
+static const tree_pos_t N_MAX_SIZE = 100001;
 static const int MAX_ALLOWED_REVENUE = 2000000000;
 static const int MIN_ALLOWED_REVENUE = 0;
-//
-//tree_val_t min_t[TREE_MAX_SIZE];
-//tree_val_t max_t[TREE_MAX_SIZE];
-//tree_val_t delta[TREE_MAX_SIZE];
+
+tree_val_t min_t[TREE_MAX_SIZE];
+tree_val_t max_t[TREE_MAX_SIZE];
+tree_val_t delta[TREE_MAX_SIZE];
 //bool calculated[TREE_MAX_SIZE];
 
 // ------------------------------------------------------------------------------------------
@@ -30,21 +30,33 @@ int main() {
     cin >> m;
 
     // TODO: Prepare state
+    // calculate growth periods in O(n)
 
     for(int I=0; I<m; I++) {
         int current_l, current_r, current_delta;
         cin >> current_l >> current_r >> current_delta;
 
-        int ans = -1;
+        int ans = -1;right delta
 
         bool too_much = false;
         for(int i=current_l; i<=current_r; i++) {
+            // check if min-max are not violated
+
             if(rev[i] + current_delta > MAX_ALLOWED_REVENUE || rev[i] + current_delta < MIN_ALLOWED_REVENUE) {
                 too_much = true;
                 break;
             }
         }
         if(!too_much) {
+            // calculate values for left and right edge and compare with previous growth periods #
+
+            // TODO
+            // calc left delta
+            // calc new left edge: bottom-up to LCA with updating subtree deltas
+            // compare new delta with old delta
+            // update growth periods #
+            // repeat for right edge -> TODO: Left and right edge have to run parallel to get to LCA in O(n)
+
             for(int i=current_l; i<=current_r; i++) {
                 rev[i] += current_delta;
             }
@@ -55,29 +67,9 @@ int main() {
                 }
             }
         }
-        cout << ans << endl;
+        // return -1 if min-max violated
 
-//        if(delta > 0) {
-//            if(get_max(current_l, current_r) + delta > MAX_ALLOWED_REVENUE) {
-//                cout << -1 << endl;
-//                continue;
-//            }
-//            set_max(current_l, current_r, delta);
-//        } else if(delta < 0) {
-//            if(get_min(current_l, current_r) + delta < MIN_ALLOWED_REVENUE) {
-//                cout << -1 << endl;
-//                continue;
-//            }
-//            set_min(current_l, current_r, delta); // update values on min path and direct neigh
-//        } else {
-//            // delta == 0
-//        }
-//        set_l; // update vabourslues in left edge and its direct neighbours
-//        set_r; // update values in right edge and its direct neighbours
-//
-//        // return calculated new delta value based on ed
-//        ans = set_positive_deltas;
-//        cout << ans << endl;
+        cout << ans << endl;
 
     }
 
