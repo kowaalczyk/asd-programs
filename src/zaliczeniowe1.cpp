e8b4749da5ff0a4be6965f2faaf91bb4bf6a88ae
Krzysztof Kowalczyk <k.kowaalczyk@gmail.com>
2017-11-11 19:17:24 +0100

    Refactor

--- src/zaliczeniowe1.cpp
+++ src/zaliczeniowe1.cpp
@@ -76,36 +76,29 @@ void tree_create(tree_val_t tree[], tree_pos_t leaves, tree_val_t base_value=0) 
 
 // INTERVAL TREE EXTENDED -------------------------------------------------------------------
 
-void tree_rebase_min(tree_val_t min_t[], tree_val_t delta_t[], tree_pos_t node_pos) {
+/// calculates minimums and maximus bottom-up starting from node_pos
+void tree_recalc(tree_pos_t node_pos) {
     if(node_pos > 1) {
         tree_pos_t parent_pos = tree_parent_pos(node_pos);
         auto lson_pos = tree_lson_pos(parent_pos);
         auto rson_pos = tree_rson_pos(parent_pos);
         min_t[parent_pos] = min((min_t[lson_pos] + delta[lson_pos]), (min_t[rson_pos] + delta[rson_pos]));
-        tree_rebase_min(min_t, delta_t, parent_pos);
-    }
-}
-
-void tree_rebase_max(tree_val_t max_t[], tree_val_t delta_t[], tree_pos_t node_pos) {
-    if(node_pos > 1) {
-        tree_pos_t parent_pos = tree_parent_pos(node_pos);
-        auto lson_pos = tree_lson_pos(parent_pos);
-        auto rson_pos = tree_rson_pos(parent_pos);
         max_t[parent_pos] = max((max_t[lson_pos] + delta[lson_pos]), (max_t[rson_pos] + delta[rson_pos]));
-        tree_rebase_max(max_t, delta_t, parent_pos);
+        tree_recalc(parent_pos);
     }
 }
 
+/// initializes trees based on array of values
 void set_minmax(const tree_val_t rev[], tree_pos_t rev_size) {
     for(tree_pos_t i = 1; i <= rev_size; i++) {
         tree_pos_t leaf = tree_pos(i-1);
         min_t[leaf] = rev[i];
-        tree_rebase_min(min_t, delta, leaf);
         max_t[leaf] = rev[i];
-        tree_rebase_max(max_t, delta, leaf);
+        tree_recalc(leaf);
     }
 }
 
+/// updates one node based on its present delta, without updating node's parents or children
 void update_if_delta_present(tree_pos_t current_pos, tree_pos_t current_left_bound, tree_pos_t current_right_bound) {
     if(delta[current_pos]) {
         // update current_pos if it needs to be updated
@@ -119,6 +112,7 @@ void update_if_delta_present(tree_pos_t current_pos, tree_pos_t current_left_bou
     }
 }
 
+/// use minmax_violated
 bool minmax_violated_recursive(
         tree_pos_t current_pos,
         tree_pos_t current_left_bound,
@@ -141,11 +135,13 @@ bool minmax_violated_recursive(
     return ans;
 }
 
+/// checks if minimum and maximum revenue requirements will not be violated after interval [l,r] is updated with val
 bool minmax_violated(tree_pos_t l, tree_pos_t r, tree_val_t val) {
     return minmax_violated_recursive(tree_root_pos(), tree_pos(0), tree_size-1, tree_pos(l-1), tree_pos(r-1), val);
 }
 
-tree_val_t update_minmax_recursive(
+/// use update_minmax
+void update_minmax_recursive(
         tree_pos_t current_pos,
         tree_pos_t current_left_bound,
         tree_pos_t current_right_bound,
@@ -154,54 +150,30 @@ tree_val_t update_minmax_recursive(
         tree_val_t val) {
     update_if_delta_present(current_pos, current_left_bound, current_right_bound);
     if(current_left_bound > current_right_bound || current_left_bound > right_bound || current_right_bound < left_bound) {
-        return 0;
+        return;
     }
     if(current_left_bound >= left_bound && current_right_bound <= right_bound) {
         // representing change of all leaves in interval by val by changing min and max in critical points
-        tree_val_t growth_delta = 0;
         min_t[current_pos] += val;
         max_t[current_pos] += val;
         if(current_left_bound != current_right_bound) {
             // not a leaf
             delta[tree_lson_pos(current_pos)] += val;
             delta[tree_rson_pos(current_pos)] += val;
-        } else {
-            // a leaf => check if difference between previous/following rev value changed sign
-            tree_val_t prev_val = min_t[current_pos] - val;
-            tree_val_t new_val = min_t[current_pos];
-            if(current_pos == left_bound && current_pos > tree_pos(0)) {
-                tree_val_t left_val = min_t[current_pos-1];
-                if(left_val < prev_val && left_val >= new_val) {
-                    growth_delta--;
-                } else if(left_val >= prev_val && left_val < new_val) {
-                    growth_delta++;
-                }
-            }
-            if(current_pos == right_bound && current_pos < tree_size-2) {
-                tree_val_t right_val = min_t[current_pos+1];
-                if(prev_val < right_val && new_val >= right_val) {
-                    growth_delta--;
-                } else if(prev_val >= right_val && new_val < right_val) {
-                    growth_delta++;
-                }
-            }
         }
-        return growth_delta;
+        return;
     }
     tree_pos_t divider = (current_left_bound + current_right_bound) / 2;
-    tree_val_t growth_delta = update_minmax_recursive(tree_lson_pos(current_pos), current_left_bound, divider, left_bound, right_bound, val);
-    growth_delta += update_minmax_recursive(tree_rson_pos(current_pos), divider+1, current_right_bound, left_bound, right_bound, val);
-
     min_t[current_pos] = min(min_t[tree_lson_pos(current_pos)], min_t[tree_rson_pos(current_pos)]);
     max_t[current_pos] = max(max_t[tree_lson_pos(current_pos)], max_t[tree_rson_pos(current_pos)]);
-
-    return growth_delta;
 }
 
-tree_val_t update_minmax(tree_pos_t l, tree_pos_t r, tree_val_t val) {
-    return update_minmax_recursive(tree_root_pos(), tree_pos(0), tree_size-1, tree_pos(l-1), tree_pos(r-1), val);
+/// updates min_t and max_t wiht val
+void update_minmax(tree_pos_t l, tree_pos_t r, tree_val_t val) {
+    update_minmax_recursive(tree_root_pos(), tree_pos(0), tree_size-1, tree_pos(l-1), tree_pos(r-1), val);
 }
 
+/// updates min and max for each of the leaves with its and its predecessors deltas
 tree_val_t update_leaf_values(const tree_pos_t leaf_pos[], tree_pos_t count) {
     //iterating though each individual element makes sure that top-down tree search reaches bottom
     for(tree_pos_t i=0; i<count; i++) {
@@ -209,6 +181,7 @@ tree_val_t update_leaf_values(const tree_pos_t leaf_pos[], tree_pos_t count) {
     }
 }
 
+/// calculates revenue growth changes afer [l,r] is updated with delta
 tree_val_t calc_growth_changes(tree_pos_t l, tree_pos_t r, tree_pos_t rev_size, tree_val_t updated_val) {
     tree_val_t ans = 0;
     if(l>1) {
@@ -242,6 +215,7 @@ tree_val_t calc_growth_changes(tree_pos_t l, tree_pos_t r, tree_pos_t rev_size, 
 
 // HELPERS ----------------------------------------------------------------------------------
 
+/// calculates number of initial growth periods
 tree_pos_t base_growth_periods(const tree_val_t rev[], tree_pos_t rev_size) {
     tree_pos_t ans = 0;
     for(tree_pos_t i = 2; i <= rev_size; i++) {
@@ -257,7 +231,7 @@ tree_pos_t base_growth_periods(const tree_val_t rev[], tree_pos_t rev_size) {
 int main() {
     tree_pos_t n, m;
     cin >> n;
-    tree_val_t rev[n+1]; // 0 input data is indexed from 1
+    tree_val_t rev[n+1]; // input data is indexed from 1
     for(tree_pos_t i = 1; i <= n; i++) {
         cin >> rev[i];
     }
