58580be68f1f19482ba4e2c76f2755efdd389518
Krzysztof Kowalczyk <k.kowaalczyk@gmail.com>
2017-11-26 21:34:25 +0100

    Solved connection storage

--- src/zaliczeniowe2.cpp
+++ src/zaliczeniowe2.cpp
@@ -16,13 +16,24 @@ using namespace std;
 
 const int MAX_NODES = 20000;
 const int MAX_COUPONS = 10;
+const int MAX_CONNECTIONS = 1000000;
 const int NODE_UNREACHABLE = -1;
 
-vector<int> G[MAX_NODES]; /// j in G[i] <==> route i->j is present in graph
-vector<int> G_pred[MAX_NODES]; /// j in G_pred[i] <==> route j->i is present in graph
-long long min_cost[MAX_COUPONS+1][MAX_NODES]; /// min_cost[k][n] := minimal cost to reach node n with k coupons
-int routes_to_calc[MAX_NODES]; /// # of direct routes to each node without calculated cost
+vector<pair<int, int>> G[MAX_NODES]; /// j is one of G[i].first <==> route i->j is present in graph. G[i].second is a connection_id
+vector<pair<int, int>> G_pred[MAX_NODES]; /// j is one of G_pred[i].first <==> route j->i is present in graph. G_pred[i].second is a connection_id
+pair<long long, long long> connections[MAX_CONNECTIONS];
+
 queue<int> Q; /// contains all nodes for which we can calculate min_cost
+int routes_to_calc[MAX_NODES]; /// # of direct routes to each node without calculated cost
+long long min_cost[MAX_COUPONS+1][MAX_NODES]; /// min_cost[k][n] := minimal cost to reach node n with k coupons
+
+long long get_cost(pair<int, int> connection) {
+    return connections[connection.second].first;
+}
+
+long long get_discount(pair<int, int> connection) {
+    return connections[connection.second].second;
+}
 
 int main() {
     // init arrays
@@ -36,13 +47,16 @@ int main() {
     // load variables
     int n, m, k; // # of cities, # of connections, # of coupons
     cin >> n >> m >> k;
-    for(int i=0; i<m; i++) {
+    for(int connection_id=0; connection_id<m; connection_id++) {
         int v, w, b, c; // from, to (indexed from 0), discount, cost without discount
         cin >> v >> w >> b >> c;
-        G[v].push_back(w);
-        G_pred[w].push_back(v);
+        // register connections in graph nodes
+        G[v].emplace_back(w, connection_id);
+        G_pred[w].emplace_back(v, connection_id);
         routes_to_calc[w]++;
-        // TODO: Find a way to cache discounts optimally
+        // register connection cost and discount
+        connections[connection_id].first = c;
+        connections[connection_id].second = b;
     }
 
     assert(routes_to_calc[0] == 0);
