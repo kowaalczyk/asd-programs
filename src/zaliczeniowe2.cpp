71e2bf950c255275b481745e11994efcd5e4f818
Krzysztof Kowalczyk <k.kowaalczyk@gmail.com>
2017-12-06 20:06:35 +0100

    1st Dijkstra iteration

--- src/zaliczeniowe2.cpp
+++ src/zaliczeniowe2.cpp
@@ -2,6 +2,7 @@
 #include <vector>
 #include <queue>
 #include <tuple>
+#include <cassert>
 
 using namespace std;
 
@@ -43,11 +44,11 @@ const int MAX_COUPONS = 10;
 const int MAX_CONNECTIONS = 1000000;
 const int NODE_UNREACHABLE = -1;
 
-typedef int id_t;
+typedef int id_t; // TODO: wtf is wrong here?
 typedef long long int cost_t;
 
-typedef tuple<id_t, id_t, id_t, cost_t, cost_t> connection_t; /// connection_id, from_node, to_node, cost, discount
-typedef pair<cost_t, id_t> connection_dijkstra_t;
+typedef tuple<id_t, id_t, id_t, cost_t, cost_t> connection_t; /// := {connection_id, from_node, to_node, cost, discount}
+typedef pair<cost_t, id_t> connection_dijkstra_t; /// := {connection_temporary_cost, connection_id} for use in Dijkstra queue
 
 connection_t connections[MAX_CONNECTIONS];
 vector<id_t> connections_from[MAX_NODES]; /// G[i] := vector of connection ids starting from i
@@ -55,15 +56,29 @@ priority_queue<connection_dijkstra_t> dijkstra_q; /// priority queue for Dijkstr
 
 cost_t min_cost[MAX_COUPONS+1][MAX_NODES]; /// min_cost[k][n] := minimal cost to reach node n with k coupons
 
+cost_t get_cost(const connection_t &connection) {
+    return get<3>(connection);
+}
+
+cost_t get_discounted_cost(const connection_t &connection) {
+    return get<3>(connection) - get<4>(connection);
+}
+
+id_t get_destination(const connection_t &connection) {
+    return get<2>(connection);
+}
+
 void load_connection(id_t connection_id) {
     int v, w, b, c;
     cin >> v >> w >> b >> c;
     connection_t new_connection {connection_id, v, w, b, c};
     connections[connection_id] = new_connection;
-    connections_from[v].push_back(connection_id);
+    connections_from[v].emplace_back(connection_id);
 }
 
 int main() {
+    ios_base::sync_with_stdio(false); // TODO: make sure this does not fuck up everything in functions above
+
     // init arrays
     for(int i=0; i<MAX_NODES; i++) {
         for(int j=0; j<=MAX_COUPONS; j++) {
@@ -74,16 +89,45 @@ int main() {
     // load variables
     int n, m, k; // # of cities, # of connection_data, # of coupons
     cin >> n >> m >> k;
+    const int first_node_id = 0;
     const int last_node_id = n-1;
-
     for(id_t connection_id=0; connection_id<m; connection_id++) {
         load_connection(connection_id);
     }
 
-    // TODO: standard Dijkstra for calculating cost with 0 discount && checking whether last_node is reachable
+    // standard Dijkstra for calculating cost with 0 discount && checking whether last_node is reachable
+    dijkstra_q.emplace(0, first_node_id);
+    while(!dijkstra_q.empty()) {
+        // TODO: Make sure break condition is optimal (spanning tree for higher k's might contain different nodes => if complexity is too high, add decrease_key)
+        auto current_node_data = dijkstra_q.top();
+        dijkstra_q.pop();
+        // get both cost variants
+        cost_t current_node_previous_cost = min_cost[0][current_node_data.second];
+        cost_t current_node_temp_cost = current_node_data.first;
+        // calculate actual cost and update values
+        cost_t current_node_calculated_cost;
+        if(current_node_previous_cost == NODE_UNREACHABLE) {
+            current_node_calculated_cost = current_node_temp_cost;
+        } else {
+            current_node_calculated_cost = min(current_node_previous_cost, current_node_temp_cost);
+        }
+        min_cost[0][current_node_data.second] = current_node_calculated_cost;
+        // add following nodes to Dijkstra queue with calculated temporary cost
+        for(id_t following_connection_id : connections_from[current_node_data.second]) {
+            connection_t &following_connection = connections[following_connection_id];
+            dijkstra_q.emplace(current_node_calculated_cost + get_cost(following_connection), get_destination(following_connection));
+        }
+    }
+
+    if(min_cost[0][last_node_id] == NODE_UNREACHABLE) {
+        // node is unreachable after standard Dijkstra ==> there is no way to reach it in further iterations
+        cout << -1 << endl;
+        exit(0);
+    }
 
     // TODO: 2nd Dijkstra for calculating other costs
+    assert(dijkstra_q.empty());
 
-    cout << min_cost << endl;
+    cout << min_cost[k][last_node_id] << endl;
     return 0;
 }
