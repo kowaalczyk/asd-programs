33c36fedd6d1d6e9aa797a89b3e3605c1a31be1d
Krzysztof Kowalczyk <k.kowaalczyk@gmail.com>
2017-12-06 17:27:49 +0100

    Dijkstra rewrite overview

--- src/zaliczeniowe2.cpp
+++ src/zaliczeniowe2.cpp
@@ -14,36 +14,68 @@ using namespace std;
  *
  */
 
+
+/*
+ * TODO - Rewrite w/ Dijkstra:
+ *
+ * for(k : # possible coupons):
+ *   - all reachable nodes have cost w/ (k-1)-discounts calculated
+ *   - all nodes in currently calculated set have cost w/ k-discounts calculated
+ *
+ *   1. current node := front of Q
+ *   2. current node cost w/ k discounts := min(its previous cost w/ k discounts, its q-top cost)
+ *      - if node n has been calculated, BREAK
+ *   3. for following node:
+ *      - Q-pushed cost of following node w/ k-discounts :=
+ *          min(cost of current node w/ (k-1) discounts + cost of reaching next w/ discount,
+ *              cost of current node w/ k-discounts + cost o reaching next w/o discount)
+ *      - push <Q-pushed cost, following node> to Q
+ *
+ *   NOTES:
+ *    - There is no decrease_key(Q) ==> need to break early (might have to implement own Q, but pre-process w/ BFS should suffice)
+ *    - Cost of reaching nodes w/o discounts can be calculated separately to prevent arr[-1] errors
+ *
+ */
+
 const int MAX_NODES = 20000;
 const int MAX_COUPONS = 10;
 const int MAX_CONNECTIONS = 1000000;
 const int NODE_UNREACHABLE = -1;
 
-vector<pair<int, int>> G[MAX_NODES]; /// j is one of G[i].first <==> route i->j is present in graph. G[i].second is a connection_id
-vector<pair<int, int>> G_pred[MAX_NODES]; /// j is one of G_pred[i].first <==> route j->i is present in graph. G_pred[i].second is a connection_id
-pair<long long, long long> connections[MAX_CONNECTIONS];
+typedef long long int cost_t;
+typedef pair<int, int> connection_with_id;
+typedef pair<cost_t , cost_t > connection_data_t;
 
+typedef tuple<id_t, id_t, id_t, cost_t, cost_t> connection_t; /// connection_id, from_node, to_node, cost, discount
+typedef pair<cost_t, id_t> connection_dijkstra_t; ///
+
+
+vector<connection_with_id> connections_from[MAX_NODES]; /// j is one of G[i].first <==> route i->j is present in graph. G[i].second is a connection_id
+vector<connection_with_id> connections_to[MAX_NODES]; /// j is one of G_pred[i].first <==> route j->i is present in graph. G_pred[i].second is a connection_id
+
+connection_data_t connection_data[MAX_CONNECTIONS]; /// connection_data[connection_id] := {connection_id.cost, connection_id.discount}
 queue<int> Q; /// contains all nodes for which we can calculate min_cost
 int routes_to_calc[MAX_NODES]; /// # of direct routes to each node without calculated cost
-long long min_cost[MAX_COUPONS+1][MAX_NODES]; /// min_cost[k][n] := minimal cost to reach node n with k coupons
 
-int get_node_id(pair<int, int> connection) {
+cost_t min_cost[MAX_COUPONS+1][MAX_NODES]; /// min_cost[k][n] := minimal cost to reach node n with k coupons
+
+int get_node_id(connection_with_id connection) {
     return connection.first;
 }
 
-long long get_cost(pair<int, int> connection) {
-    return connections[connection.second].first;
+cost_t get_cost(connection_data_t connection) {
+    return connection_data[connection.second].first;
 }
 
-long long get_discount(pair<int, int> connection) {
-    return connections[connection.second].second;
+cost_t get_discount(connection_data_t connection) {
+    return connection_data[connection.second].second;
 }
 
-long long calc_min_cost(int node_id) {
-    long long ans = -1;
+cost_t calc_min_cost(int node_id) {
+    cost_t ans = NODE_UNREACHABLE;
     for(int coupons = MAX_COUPONS; coupons >= 0; coupons--) {
-        if(min_cost[coupons][node_id] != -1) {
-            if(ans == -1) {
+        if(min_cost[coupons][node_id] != NODE_UNREACHABLE) {
+            if(ans == NODE_UNREACHABLE) {
                 ans = min_cost[coupons][node_id];
             } else {
                 ans = min(ans, min_cost[coupons][node_id]);
@@ -65,19 +97,19 @@ int main() {
     }
 
     // load variables
-    int n, m, k; // # of cities, # of connections, # of coupons
+    int n, m, k; // # of cities, # of connection_data, # of coupons
     cin >> n >> m >> k;
     int last_node_id = n-1;
 
     for(int connection_id=0; connection_id<m; connection_id++) {
         int v, w, b, c; // from, to (indexed from 0), discount, cost without discount
         cin >> v >> w >> b >> c;
-        // register connections in graph nodes
-        G[v].emplace_back(w, connection_id);
-        G_pred[w].emplace_back(v, connection_id);
+        // register connection_data in graph nodes
+        connections_from[v].emplace_back(w, connection_id);
+        connections_to[w].emplace_back(v, connection_id);
         // register connection cost and discount
-        connections[connection_id].first = c;
-        connections[connection_id].second = b;
+        connection_data[connection_id].first = c;
+        connection_data[connection_id].second = b;
     }
 
     // first bfs maps possible routes from node 0 to each of the nodes
@@ -86,7 +118,7 @@ int main() {
     while(!Q.empty()) {
         int current_node_id = Q.front();
         Q.pop();
-        for(auto connection : G[current_node_id]) {
+        for(auto connection : connections_from[current_node_id]) {
             int following_node_id = get_node_id(connection);
             if(routes_to_calc[following_node_id] == 0 && following_node_id != 0) {
                 Q.push(following_node_id);
@@ -109,18 +141,18 @@ int main() {
         Q.pop();
         assert(routes_to_calc[current_node_id] == 0);
         // if we are calculating first node, set all costs to 0
-        if(G_pred[current_node_id].empty()) {
+        if(connections_to[current_node_id].empty()) {
             for(int coupons=0; coupons< MAX_COUPONS; coupons++) {
                 min_cost[coupons][current_node_id] = 0;
             }
         }
         // calculate cost without discounts
-        for(pair<int, int> & connection : G_pred[current_node_id]) {
+        for(pair<int, int> & connection : connections_to[current_node_id]) {
             min_cost[0][current_node_id] = min_cost[0][get_node_id(connection)] + get_cost(connection);
         }
         // calculate cost with discounts
         for(int coupons=1; coupons <= MAX_COUPONS; coupons++) {
-            for(auto connection : G_pred[current_node_id]) {
+            for(auto connection : connections_to[current_node_id]) {
                 long long cost_with_discounted_prev = min_cost[coupons][get_node_id(connection)] + get_cost(connection);
                 long long cost_with_discounted_curr = min_cost[coupons-1][get_node_id(connection)] + get_cost(connection) - get_discount(connection);
                 min_cost[coupons][current_node_id] = min(cost_with_discounted_prev, cost_with_discounted_curr);
@@ -131,7 +163,7 @@ int main() {
             break;
         }
         // add next nodes to queue if possible
-        for(auto connection : G[current_node_id]) {
+        for(auto connection : connections_from[current_node_id]) {
             int following_node_id = get_node_id(connection);
             routes_to_calc[following_node_id]--;
             if(routes_to_calc[following_node_id] == 0) {
