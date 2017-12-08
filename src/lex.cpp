4bf11a5ea364628dba9be602c50345dc74f071f4
Krzysztof Kowalczyk <k.kowaalczyk@gmail.com>
2017-12-08 12:36:35 +0100

    Notes from class, not complete

---
+++ src/lex.cpp
@@ -0,0 +1,8 @@
+/*
+ * TODO
+ * Zadanie ze słownikami:
+ * - słownik podsłów bazowych - struktura pamiętająca kolejność podsłów występujących na wejściu
+ * - sprawdzamy pozycje danych słów w słowniku i porównujemy ich pierwsze litery (???)
+ *
+ * Preprocessing i zwracanie wyników W CZASIE STAŁYM
+ */
