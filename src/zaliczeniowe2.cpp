42d7d9e753edd521bd411edec07f38670e04ed65
Krzysztof Kowalczyk <k.kowaalczyk@gmail.com>
2017-12-08 15:41:17 +0100

    Refactor

--- src/zaliczeniowe2.cpp
+++ src/zaliczeniowe2.cpp
@@ -1,13 +1,12 @@
 #include <iostream>
 #include <vector>
 #include <queue>
-#include <tuple>
 #include <cassert>
 
 using namespace std;
 
 /*
- * TODO: Try alternative solution:
+ * Alternative solution:
  *
  * 1) For each city in input create (k+1) nodes in graph (for each level of discounts used to reach the node)
  * 2) For each connection v->w for each level in 0..k create edge in the level and create an edge to higher level (representing more discounts used)
@@ -23,29 +22,28 @@ typedef pair<cost_t, graph_id_t> connection_t; /// connection := <cost of connec
 
 const graph_id_t MAX_NODES = 20000;
 const int MAX_COUPONS = 10;
-const graph_id_t MAX_CONNECTIONS = 100000;
-const cost_t NODE_UNREACHABLE = -1;
 
-graph_id_t start_id = -1;
-graph_id_t end_id = -1;
 level_t graph_max_level = 0; /// # of discount levels available (0 := no discounts, k := 0..k discounts can be used)
 graph_id_t graph_level_size = 0; /// size of each level
-vector<connection_t> connections_from[MAX_NODES*(MAX_COUPONS+1)];
-priority_queue<connection_t, vector<connection_t>, greater<connection_t>> dijkstra_q;
-bool visited[MAX_NODES*(MAX_COUPONS+1)];
+vector<connection_t> connections_from[MAX_NODES*(MAX_COUPONS+1)]; /// connections_from[n] := vector of connections from node n
+priority_queue<connection_t, vector<connection_t>, greater<connection_t>> dijkstra_q; /// priority queue for Dijkstra algorithm
+graph_id_t start_id = -1; /// id of starting node in Dijkstra algorithm
+graph_id_t end_id = -1; /// id of finishing node in Dijkstra algorithm
+bool visited[MAX_NODES*(MAX_COUPONS+1)]; /// flag for visited nodes
 
+/// for a given city position, get its node within the graph on specified level
 graph_id_t graph_pos(graph_id_t node_id, level_t level) {
     assert(graph_level_size > 0);
-
     return graph_level_size*level + node_id;
 }
 
+/// for a given graph node, get its city position (position with relation to its level)
 graph_id_t level_pos(graph_id_t node_id) {
     assert(graph_level_size > 0);
-
     return node_id % graph_level_size;
 }
 
+/// set up variables necessary for graph traversal
 void create_graph(graph_id_t level_size, level_t levels) {
     // set variables
     graph_max_level = levels;
@@ -59,6 +57,7 @@ void create_graph(graph_id_t level_size, level_t levels) {
     }
 }
 
+/// read connection information from stdin and update graph data
 void load_connection() {
     assert(graph_level_size > 0);
 
@@ -74,6 +73,7 @@ void load_connection() {
     connections_from[graph_pos(from, graph_max_level)].emplace_back(cost, graph_pos(target, graph_max_level)); // connection w/o discount at max level
 }
 
+/// search for optimal path from start_id to end_id in a graph using Dijkstra algorithm
 cost_t dijkstra_search() {
     assert(graph_level_size > 0);
     assert(start_id == 0);
@@ -88,7 +88,7 @@ cost_t dijkstra_search() {
         cost_t current_cost = current_data.first;
         graph_id_t current_node = current_data.second;
         visited[current_node] = true;
-        //
+        // last node was reached, we can stop here
         if(level_pos(current_node) == end_id) {
             return current_cost;
         }
