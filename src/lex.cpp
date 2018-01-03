852324394f60bf25b442ae45ae8777a8a5e7bbb1
Krzysztof Kowalczyk <k.kowaalczyk@gmail.com>
2018-01-03 16:18:44 +0100

    lex_sort implementation passing basic test

--- src/lex.cpp
+++ src/lex.cpp
@@ -1,13 +1,7 @@
-/*
- * TODO
- * Zadanie ze słownikami:
- * - słownik podsłów bazowych - struktura pamiętająca kolejność podsłów występujących na wejściu
- * - sprawdzamy pozycje danych słów w słowniku i porównujemy ich pierwsze litery (???)
- *
- * Preprocessing i zwracanie wyników W CZASIE STAŁYM
- */
-
 #include <iostream>
+#include <vector>
+#include <tuple>
+#include <algorithm>
 #include <cassert>
 
 using namespace std;
@@ -15,15 +9,95 @@ using namespace std;
 const int N_MAX = 300000;
 const int N_LOG_MAX = 20; // more than enough
 
-string word;
-int order[N_MAX][N_LOG_MAX];
+string word = "";
+int order[N_LOG_MAX][N_MAX]; /// order[subword_length][subword_start_pos] := i <==> subword is i-th largest among its length
+
+
+/// returns floor of log_2(x)
+inline int log_floor(int x) {
+    assert(x>0);
+    int ans = 0;
+    while (x >>= 1) ++ans;
+    return ans;
+}
+
+/// returns 2^x
+inline int pow_2(int x) {
+    assert(x>=0);
+    return (x > 0 ? 2<<(x-1) : 1);
+}
+
+/// populates order array for faster comparison of word substrings, requires word to be loaded.
+void process_word(int word_size) {
+    assert(word != "");
+    int size_log = log_floor(word_size);
+
+    // level 0
+    for(int j=0; j<word_size; j++) {
+        order[0][j] = word[j]; // casting char to int
+    }
+
+    // further levels
+    for(int i=1; i<=size_log; i++) { // TODO: \gt or \geq ?
+        vector<tuple<int, int, int>> prev_order;
+        int prev_length = pow_2(i-1);
+        for(int j=0; j<word_size; j++) {
+            prev_order.emplace_back(order[i-1][j], order[i-1][j+prev_length], j);
+        }
+        sort(prev_order.begin(), prev_order.end());
+
+        int current_order_position = 0; // position of currently accessed word =
+        for(int j=0; j<word_size; j++) {
+            int order_index = get<2>(prev_order.at((unsigned int)j));
+            if(j>0) {
+                if(get<0>(prev_order.at((unsigned int)(j))) == get<0>(prev_order.at((unsigned int)(j-1))) && get<0>(prev_order.at((unsigned int)(j))) == get<0>(prev_order.at((unsigned int)(j-1)))) {
+                    // new order position is same as for previous element because they are equal
+                } else {
+                    current_order_position++;
+                }
+
+            }
+            order[i][order_index] = current_order_position;
+        }
+    }
+}
 
 /// compares substring word[a..b] with word[c..d], returns '<', '>' or '='
 char lex_compare(int a, int b, int c, int d) {
-    // TODO
+    int len_left = b-a+1;
+    int len_right = d-c+1;
 
-    return 'x';
+    assert(0 < len_left);
+    assert(0 < len_right);
+
+    int left_logfloor = log_floor(len_left);
+    int right_logfloor = log_floor(len_right);
+
+    int min_level = min(left_logfloor, right_logfloor);
+    int min_level_length = pow_2(min_level);
+    int min_level_cmp = order[min_level][a] - order[min_level][c];
+
+    assert(min_level_length <= len_left);
+    assert(min_level_length <= len_right);
+
+    if(min_level_cmp < 0) {
+        return '<';
+    } else if(min_level_cmp > 0) {
+        return '>';
+    } else if(min_level_length == len_left && min_level_length == len_right) {
+        // cmp==0 and both sides are equal on full length
+        return '=';
+    } else if(min_level_length == len_right) {
+        // cmp==0 and compared on full right length
+        return '>';
+    } else if(min_level_length == len_left) {
+        // cmp==0 and compared on full left length
+        return '<';
+    }
+    // need to compare further
+    return lex_compare(a+min_level_length, b, c+min_level_length, d);
 }
+
 
 int main() {
     ios_base::sync_with_stdio(false);
@@ -37,17 +111,17 @@ int main() {
     // load word
     int n, m;
     cin >> n >> m;
-    // TODO: Make sure loading to string is fine
     cin >> word;
-    assert(word.length() == n);
 
-    // TODO: Pre-process word
+    // prepare word for queries
+    assert(word.length() == (unsigned int)n);
+    process_word(n);
 
-    // load queries
+    // load queries and print answers
     for(int i=0; i<m; i++) {
         int a, b, c, d;
         cin >> a >> b >> c >> d;
-         cout << lex_compare(a, b, c, d) << endl;
+         cout << lex_compare(a-1, b-1, c-1, d-1) << endl;
     }
     return 0;
 }
