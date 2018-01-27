7d9ea6cc537aae8d353dba87ad06ca3f28c8b9ac
Krzysztof Kowalczyk <k.kowaalczyk@gmail.com>
2018-01-27 11:42:06 +0100

    76/100 cleaner

--- src/exam/perly.cpp
+++ src/exam/perly.cpp
@@ -14,7 +14,6 @@ using namespace std;
 typedef uint64_t val_t;
 
 vector<pair<val_t, size_t>> pearls; // value, pos
-size_t used[100000]; // used positions
 
 int main() {
     size_t n;
@@ -30,58 +29,58 @@ int main() {
 
     sort(pearls.begin(), pearls.end(), std::greater<pair<val_t, size_t >>());
 
-//    unordered_set<size_t> used_positions;
-//    for (auto p : pearls) {
-//        if (used_positions.find(p.second-1) != used_positions.end()) {
-//            // already taken an element at the previous position
-//            if (used_positions.find(p.second-2) != used_positions.end()) {
-//                // 3rd element in a row
-//                if (!taken_three &&
-//                        used_positions.find(p.second-3) == used_positions.end() &&
-//                        used_positions.find(p.second+1) == used_positions.end()) {
-//                    ans += p.first;
-//                    used_positions.emplace(p.second);
-//                    taken_three = true;
-//                }
-//
-//            } else if (used_positions.find(p.second+1) != used_positions.end()) {
-//                // one element before and at least one after
-//
-//                if (used_positions.find(p.second+2) != used_positions.end()) {
-//                    // unable to insert - would be 4 elements in a row
-//                } else {
-//                    // exactly one before and exactly one after
-//                    if (!taken_three) {
-//                        ans += p.first;
-//                        used_positions.emplace(p.second);
-//                        taken_three = true;
-//                    }
-//                }
-//            } else {
-//                // exactly one before, no after
-//                ans += p.first;
-//                used_positions.emplace(p.second);
-//            }
-//        } else if (used_positions.find(p.second+1) != used_positions.end()) {
-//            // none before, one or more after
-//            if (used_positions.find(p.second+2) != used_positions.end()) {
-//                // none before, two or more after
-//                if (!taken_three && used_positions.find(p.second+3) == used_positions.end()) {
-//                    ans += p.first;
-//                    used_positions.emplace(p.second);
-//                    taken_three = true;
-//                }
-//            } else {
-//                // no before, exactly one after
-//                ans += p.first;
-//                used_positions.emplace(p.second);
-//            }
-//        } else {
-//            // standalone element
-//            ans += p.first;
-//            used_positions.emplace(p.second);
-//        }
-//    }
+    val_t ans = 0;
+    bool taken_three = false;
+    unordered_set<size_t> used_positions;
+    for (auto p : pearls) {
+        if (used_positions.count(p.second-1)) {
+            // already taken an element at the previous position
+            if (used_positions.count(p.second-2)) {
+                // 3rd element in a row
+                if (!taken_three && used_positions.count(p.second-3) && !used_positions.count(p.second+1)) {
+                    ans += p.first;
+                    used_positions.emplace(p.second);
+                    taken_three = true;
+                }
+
+            } else if (used_positions.count(p.second+1)) {
+                // one element before and at least one after
+
+                if (used_positions.count(p.second+2)) {
+                    // unable to insert - would be 4 elements in a row
+                } else {
+                    // exactly one before and exactly one after
+                    if (!taken_three) {
+                        ans += p.first;
+                        used_positions.emplace(p.second);
+                        taken_three = true;
+                    }
+                }
+            } else {
+                // exactly one before, no after
+                ans += p.first;
+                used_positions.emplace(p.second);
+            }
+        } else if (used_positions.count(p.second+1)) {
+            // none before, one or more after
+            if (used_positions.count(p.second+2)) {
+                // none before, two or more after
+                if (!taken_three && !used_positions.count(p.second+3)) {
+                    ans += p.first;
+                    used_positions.emplace(p.second);
+                    taken_three = true;
+                }
+            } else {
+                // no before, exactly one after
+                ans += p.first;
+                used_positions.emplace(p.second);
+            }
+        } else {
+            // standalone element
+            ans += p.first;
+            used_positions.emplace(p.second);
+        }
+    }
 
     printf("%lu\n", ans);
     return 0;
